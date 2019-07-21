c96b930544f3b5d84ea93edc04b239f6c4b9dc34
Daniel Shell <dshell321@gmail.com>
2019-07-21 13:16:09 +1000

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nd-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'neuro-des-web-angular';
+}
