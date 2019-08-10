8ce198094d2d0a644e335d83a9fce8a4430f74a7
Daniel Shell <dshell321@gmail.com>
2019-08-10 16:47:14 +1000

    + SimActivate: allow next step when prev completed

---
+++ src/app/simulation/sim-activate.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { DataService } from './data.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SimActivateGuard implements CanActivate {
+    constructor(private ds : DataService, private router : Router) {
+
+    }
+
+    canActivate(
+        next: ActivatedRouteSnapshot,
+        state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+        switch(next.url[0].path) {
+            case '1':
+                return true;
+            case '2':
+                return this.ds.startCompleted ? true : this.router.parseUrl('/sim/1');
+            case '3':
+                return this.ds.mapCompleted ? true : this.router.parseUrl('/sim/2');
+            case '4':
+                return this.ds.paramsCompleted ? true : this.router.parseUrl('/sim/3');
+            default:
+                return false;
+        }
+  }
+
+}
--- src/app/simulation/simulation.routes.ts
+++ src/app/simulation/simulation.routes.ts
@@ -1,3 +1,4 @@
+import { SimActivateGuard } from './sim-activate.guard';
 import { Routes } from '@angular/router';
 
 import { LoadComponent } from './load/load.component';
@@ -6,9 +7,9 @@ import { ParamsComponent } from './params/params.component';
 import { RunComponent } from './run/run.component';
 
 export const SIM_ROUTES: Routes = [
-    { path: '', redirectTo: '1', pathMatch: 'full' },
-    { path: '1', component: LoadComponent },
-    { path: '2', component: MapComponent },
-    { path: '3', component: ParamsComponent },
-    { path: '4', component: RunComponent }
+    { path: '', redirectTo: '1', pathMatch: 'full', canActivate: [SimActivateGuard] },
+    { path: '1', component: LoadComponent, canActivate: [SimActivateGuard]  },
+    { path: '2', component: MapComponent, canActivate: [SimActivateGuard]  },
+    { path: '3', component: ParamsComponent, canActivate: [SimActivateGuard]  },
+    { path: '4', component: RunComponent, canActivate: [SimActivateGuard]  }
 ];
